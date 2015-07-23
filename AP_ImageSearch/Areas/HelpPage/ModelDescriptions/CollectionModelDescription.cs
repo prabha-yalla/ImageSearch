df7c44a590693c131bf5f018f6d4df49e85b4aec
prabha-yalla <prabha.yalla@gmail.com>
2015-07-22 23:00:10 -0400

    rename

--- AP_ImageSearch/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace AP_ImageSearch.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
