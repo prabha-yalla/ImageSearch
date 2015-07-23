049e3ed185dac8ad25c528ad5a961da50e21018a
prabha-yalla <prabha.yalla@gmail.com>
2015-07-23 10:55:24 -0400

    initial commit

---
+++ AP_ImageSearch/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AP_ImageSearch.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AP_ImageSearch/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AP_ImageSearch.Startup))]
+
+namespace AP_ImageSearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
